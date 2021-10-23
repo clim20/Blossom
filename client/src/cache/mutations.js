c3466107e86117679833b76ddcb2b9b263b41a27
Caren Lim <carenlim20@gmail.com>
2021-10-23 17:43:26 -0400

    Removed code relating to register

--- client/src/cache/mutations.js
+++ client/src/cache/mutations.js
@@ -13,27 +13,6 @@ export const LOGIN_USER = gql`
     }
 `;
 
-export const REGISTER_USER = gql`
-    mutation register (
-        $username: String!
-        $email: String!
-    ) {
-        register(
-            registerInput: {
-                username: $username
-                email: $email
-            }
-        ) {
-            id
-            username
-            email
-            scores
-            createdAt
-            token
-        }
-    }
-`
-
 export const UPDATE_SCORE = gql`
     mutation updateScore($id: ID!, $score: Int!) {
         updateScore(id: $id, score: $score) {
