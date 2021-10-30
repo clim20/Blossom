f04d0ddd7dbab4fc8e05ac264e0641fab1202e46
Junghwa Yoon <junghwa.yoon@stonybrook.edu>
2021-10-29 20:54:37 -0400

    Update

--- client/src/pages/Update.js
+++ client/src/pages/Update.js
@@ -1,13 +1,45 @@
 import React, { useContext, useState } from 'react';
-//import { useMutation, useQuery } from '@apollo/react-hooks';
+import { useParams } from "react-router-dom";
+import { useMutation, useQuery } from '@apollo/react-hooks';
+import * as queries from '../cache/queries';
+import * as mutations from '../cache/mutations';
+import { AuthContext } from '../context/auth';
 
 import MenuBar from '../components/MenuBar';
 
 const Update = () => {
+    let userRef = React.useRef();
+    let inputRef = React.useRef('');
+
+    const { user } = useContext(AuthContext);
+    const params = useParams();
+    const userId = params ? params.userId : 'could not get params';
+
+    const { data } = useQuery(queries.GET_USERS);
+
+    var user = {};
+    if (data) {
+        user = data.findUserById;
+    }
+    
+    const handleChange = (newUsername) => {
+        if(data.includes(user.id)){
+            console.log('The username is already taken')
+        }else{
+            useMutation(mutations.UPDATE_USERNAME(user.id, newUsername));
+        }
+    }
 
     return (
         <div>
-            Update Page
+            <MenuBar/>
+            <div className="update-account">Update Account</div>
+            <div className="username">Username</div>
+            <div className="username-1"></div>
+            <div className="new-username">New Username</div>
+            <input type="text" ref={inputRef}/>
+            <button onClick={() => handleChange}>Update Username</button>
+            <Username ref={userRef}/>
         </div>
     );
 }
